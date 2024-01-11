e9f67d7d324bc83e2aaf23a83eb511cd4738b657
MyUsername <figusgiacomo04@gmail.com>
2024-01-11 15:37:47 +0100

    snack-1

---
+++ snack-1/main.js
@@ -0,0 +1,19 @@
+/*
+Blocco 1:
+Blocco 1 - JSnack 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+let number1 = Number(prompt("Inserisci un numero"))
+let number2 = Number(prompt("Inserisci un numero"))
+let number3 = Number(prompt("Inserisci un numero"))
+let number4 = Number(prompt("Inserisci un numero"))
+let number5 = Number(prompt("Inserisci un numero"))
+let number6 = Number(prompt("Inserisci un numero"))
+let number7 = Number(prompt("Inserisci un numero"))
+let number8 = Number(prompt("Inserisci un numero"))
+let number9 = Number(prompt("Inserisci un numero"))
+let number10 = Number(prompt("Inserisci un numero"))
+
+document.writeln(number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10);
